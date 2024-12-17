b35d08e38d34e8b6132412bc669ac296570745f9
DouglasFMiyamoto <89319520+DouglasFMiyamoto@users.noreply.github.com>
2024-12-17 09:28:25 -0300

    Update main.tf (#5)

--- db-infra/main.tf
+++ db-infra/main.tf
@@ -1,13 +1,14 @@
 data "aws_subnets" "private_subnets" {
   filter {
-    name   = "tag:Environment"
-    values = ["private"]
+    name   = "tag:Environment" 
+    values = ["private"]      
   }
 }
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
-  name       = "rds-subnet-group"
-  subnet_ids = data.aws_subnet_ids.private_subnets.ids
+  name        = "rds-subnet-group"
+  description = "Subnet group for RDS"
+  subnet_ids  = data.aws_subnets.private_subnets.ids
 
   tags = {
     Name = "rds-subnet-group"
@@ -32,21 +33,25 @@ resource "aws_security_group" "rds_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "rds-security-group"
+  }
 }
 
 resource "aws_db_instance" "postgres" {
-  allocated_storage    = 20
-  storage_type         = "gp3"
-  engine               = "postgres"
-  engine_version       = "16"
-  instance_class       = "db.t4g.micro"
-  name                 = "dbLanchonete"
-  username             = var.db_username
-  password             = var.db_password
-  publicly_accessible  = false
-  skip_final_snapshot  = true
+  allocated_storage      = 20
+  storage_type           = "gp3"
+  engine                 = "postgres"
+  engine_version         = "16"
+  instance_class         = "db.t4g.micro"
+  name                   = "dbLanchonete"
+  username               = var.db_username
+  password               = var.db_password
+  publicly_accessible    = false
+  skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
 
   tags = {
     Name = "LanchoneteRDS"
