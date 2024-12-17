7fcdf61ffdc1809a30a3ab9e497130d8c80d8f3d
DouglasFMiyamoto <89319520+DouglasFMiyamoto@users.noreply.github.com>
2024-12-17 09:47:55 -0300

    Update main.tf

--- db-infra/main.tf
+++ db-infra/main.tf
@@ -1,37 +1,66 @@
-data "aws_subnets" "private_subnets" {
-  filter {
-    name   = "tag:Environment"
-    values = ["private"]      
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
   }
 }
 
+# Subnet pública
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+# Subnet privada
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Private Subnet"
+  }
+}
+
+# Grupo de subnets do RDS
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name        = "rds-subnet-group"
   description = "Subnet group for RDS"
-  subnet_ids  = data.aws_subnets.private_subnets.ids
+  subnet_ids  = [aws_subnet.private_subnet.id]  # Usando a subnet privada
 
   tags = {
     Name = "rds-subnet-group"
   }
 }
 
+# Grupo de segurança para o RDS
 resource "aws_security_group" "rds_sg" {
   name        = "rds-security-group"
   description = "Allow RDS access"
-  vpc_id      = var.vpc_id
+  vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = var.eks_private_cidrs
+    cidr_blocks = ["10.0.0.0/16"]  # Permitindo acesso interno dentro da VPC
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Permitindo saída para qualquer destino
   }
 
   tags = {
@@ -39,13 +68,14 @@ resource "aws_security_group" "rds_sg" {
   }
 }
 
+# Instância do RDS
 resource "aws_db_instance" "postgres" {
   allocated_storage      = 20
   storage_type           = "gp3"
   engine                 = "postgres"
   engine_version         = "16"
   instance_class         = "db.t4g.micro"
-  db_name                = "dbLanchonete" 
+  db_name                = "dbLanchonete"
   username               = var.db_username
   password               = var.db_password
   publicly_accessible    = false
@@ -56,4 +86,9 @@ resource "aws_db_instance" "postgres" {
   tags = {
     Name = "LanchoneteRDS"
   }
+}
+
+# Output para exibir o endpoint do banco de dados
+output "db_endpoint" {
+  value = aws_db_instance.postgres.endpoint
 }
